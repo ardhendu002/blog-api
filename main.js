5a042d91c0f907ba1ae17d106a379c652db84ea4
ardhendu <ardhendupurnendu@gmail.comgit config --list>
2025-08-24 19:58:42 +0530

    added more features

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const { time } = require("console");
 const express = require("express");
-const moongose = require("mongoose");
+const mongoose = require("mongoose");
 
 
-mongoose.connect("");
+mongoose.connect("mongodb+srv://ardhendumishra2006:subham002@cluster0.pdhy5so.mongodb.net/");
 
-const schema = new moongose.schema({
+const schema = new mongoose.Schema({
     tittle: String,
     author: String,
     content: String,
@@ -33,14 +32,14 @@ app.get("/api/articles", async (req,res)=>{
     const articles = await article.find(filter).sort({time: -1})
     res.json(articles);
     }
-    catch{
+    catch(err){
         res.status(400).json({
             msg: "Sorry some error occured"
         })
     }
 })
 
-app.get("api/articles/:id", async(req,res)=>{
+app.get("/api/articles/:id", async(req,res)=>{
     try{
         const findarticle = article.findOne({Id: req.params.id});
         if(!findarticle)
@@ -51,7 +50,58 @@ app.get("api/articles/:id", async(req,res)=>{
             res.json(findarticle);
         }
     }
-    catch{
+    catch(err){
         res.json(500).json({msg: "some error occured"})
     }
 })
+
+app.post("/api/articles", async(req,res)=>{
+    try{
+        const newarticle = new article(req.body);
+        const savedarticle = await newarticle.save();
+        res.status(201).json(savedarticle);
+    }
+    catch(err){
+        res.status(500).json({msg:"couldnt save the article"})
+    }
+})
+
+app.put("/api/articles/:id", async(req,res)=>{
+    try{
+        const toupdate = await article.findOneAndUpdate(
+            {Id: req.params.id},
+            req.body,
+            {new: true , runValidators:true}
+        )
+        if(!toupdate){
+            res.status(400).json({msg:"some err occ"})
+        }
+        else{
+        res.status(202).json(toupdate)
+        }
+    }
+    catch(err){
+        res.status(500).json({msg: "some error occured"});
+    }
+})
+
+app.delete("/api/article/:id",async(req,res)=>{
+    try{
+        const todelete = await article.findOneAndDelete(
+            {ID : req.params.id},
+        )
+        if(!todelete)
+        {
+            res.status(400).json({msg: "please pass correct id"})
+        }
+        else
+        {
+            res.status(200).json({msg: "your article is deleted"});
+        }
+    }
+    catch(err){
+        res.status(500).json({msg: "some err occured"});
+    }
+})
+
+app.listen(3000);
